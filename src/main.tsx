46aa179628880f2ac07cd506f18b9332fc13825a
JULESlois <hxdccw@gmail.com>
2026-09-03 04:39:26 +0800

    feat: activate collection connected motion

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './collection-motion'
 import './tokens.css'
 import './styles.css'
 import './interaction.css'
