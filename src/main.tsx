5872b132a09c34e532dd44b1ce6ab274a0264464
JULESlois <hxdccw@gmail.com>
2026-09-04 05:26:10 +0800

    feat: load transient exit lifecycle

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './transient-lifecycle'
 import './collection-motion'
 import './layout-motion'
 import './tokens.css'
